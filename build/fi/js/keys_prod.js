4b4a10eb3447fd906a21f3811a287bf50399d64c
jonkan <joel.engstrom17@gmail.com>
2025-01-29 10:32:45 +0100

    Remove build directory from Git tracking

--- build/fi/js/keys_prod.js
+++
@@ -1,4 +0,0 @@
-const response = await fetch('/.netlify/functions/get-api-keys');
-const keys = await response.json();
-export const MAPBOX_API_KEY = keys.MAPBOX_API_KEY;
-export const FIREBASE_API_KEY = keys.FIREBASE_API_KEY;
--- build/se/js/index.js
+++
@@ -1,3 +0,0 @@
-/* Put all logic here the selects and alters elements in the dom on the <name>.html page. i.e: 
-const someVariablew documents.queryselector(some element)
-someVariable.style(color=#FFF)  */
